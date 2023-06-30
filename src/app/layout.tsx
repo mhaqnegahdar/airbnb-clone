27406d0828a8bfd9b169122217d7929f7e1ec5da
max jn <maxjn763@gmail.com>
2023-06-30 06:08:49 +0330

    Fixes: - Removed unnccessary imports - Added a faveicon - Added some links as refrence - Added **Footer** component and changed **body style** to have a good looking footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,15 @@
 import "./globals.css";
 import { Nunito } from "next/font/google";
-import { ContainerProps } from "@/types";
+
+//Components
 import NavBar from "@/components/navbar/NavBar";
 import OuterProviders from "@/components/Providers/OuterProviders";
 import InnerProviders from "@/components/Providers/InnerProviders";
+import Footer from "@/components/Footer";
+//Actons
 import getCurrentUser from "@/actions/getCurrentUser";
+//Types
+import { ContainerProps } from "@/types";
 
 const nunito = Nunito({ subsets: ["latin"] });
 
@@ -19,10 +24,14 @@ export default async function RootLayout({ children }: ContainerProps) {
   return (
     <OuterProviders>
       <html lang="en">
-        <body className={nunito.className} suppressHydrationWarning={true}>
+        <body
+          className={`${nunito.className} flex flex-col min-h-screen `}
+          suppressHydrationWarning={true}
+        >
           <NavBar currentUser={currentUser} />
           <div className="pb-20 pt-28">{children}</div>
           <InnerProviders />
+          <Footer />
         </body>
       </html>
     </OuterProviders>
